939105b980d1bb14b02f234682ef13e4be0dae8d
Alvaro Gouveia <agouveialins@gmail.com>
2023-12-22 00:41:01 -0300

    Avatar component

--- Project-01/src/components/Sidebar.jsx
+++ Project-01/src/components/Sidebar.jsx
@@ -1,3 +1,4 @@
+import {Avatar} from './Avatar'
 import styles from './Sidebar.module.css'
 
 import { PencilLine } from 'phosphor-react'
@@ -8,7 +9,7 @@ export function Sidebar(){
       <img className={styles.cover} src="https://images.unsplash.com/photo-1488590528505-98d2b5aba04b?ixid=M3w0OTI1NTZ8MHwxfHNlYXJjaHwxfHxkZXZ8ZW58MHwwfHx8MTY5Mjc0MzA0N3ww&ixlib=rb-4.0.3"/>
       <div className={styles.profile}>
 
-        <img className={styles.avatar} src="https://github.com/agouveiabr.png" alt="Foto de perfil" />
+        <Avatar src="https://github.com/agouveiabr.png"/>
 
         <strong>Alvaro Gouveia</strong>
         <span>Web Developer</span>
